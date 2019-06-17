9a979740f6f40d26f965db3dcb4a8c13b30b726c
MichaelD94 <maicondouglaspaim94@gmail.com>
2019-06-17 16:04:23 -0300

    paginação pra 9 itens

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,44 +1,44 @@
 const mongoose = require('mongoose');
 
 const Product = mongoose.model('Product');
-const File = require ('../models/File');
+const File = require('../models/File');
 
 module.exports = {
     async index(req, res) {
         const { page = 1 } = req.query;
-        const products = await Product.paginate({/* condições where e etc*/}, {page, limit: 10 });        
+        const products = await Product.paginate({/* condições where e etc*/ }, { page, limit: 9 });
         return res.json(products);
     },
 
-    async show(req, res){
+    async show(req, res) {
         const product = await Product.findById(req.params.id).populate({
             path: 'files',
-            options: { sort : {createdAt: -1 } }
+            options: { sort: { createdAt: -1 } }
         })
 
         return res.json(product);
     },
 
-    async store(req, res){
+    async store(req, res) {
         const product = await Product.create(req.body);
 
         return res.json(product);
     },
 
-    async update(req, res){
+    async update(req, res) {
         const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-             new: true 
-            });
+            new: true
+        });
 
         return res.json(product);
     },
 
-    async destroy(req, res){
+    async destroy(req, res) {
         const product = await Product.findById(req.params.id);
 
         await File.findByIdAndRemove(product.files);
         await Product.findByIdAndRemove(req.params.id);
-        
+
         return res.send();
     }
 };
